900b2123d9308b555a351642a4ea904045857754
Sudheer-reactjs <info@auspicioussoft.com>
2024-08-26 17:55:16 +0530

    banner-new

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <div>
        <main className="pt-[82px] bg-[#F7F7F7] bg-top relative z-[1] pb-[180px]">
-      <h1>login</h1>
+      <h1>login page</h1>
       </main>
     </div>
   )
